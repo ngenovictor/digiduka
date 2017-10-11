7c16f1390d591c99802375d4a55aa908f0d611c6
waracci <morriswarachi0@gmail.com>
2017-10-11 17:13:53 +0300

    it will add the users model

---
+++ app/src/main/java/com/digiduka/digiduka/models/User.java
@@ -0,0 +1,32 @@
+package com.digiduka.digiduka.models;
+
+import java.util.Date;
+
+/**
+ * Created by waracci on 10/11/17.
+ */
+
+public class User {
+    private String name;
+    private String shopName;
+    private Date date;
+
+    public User() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getShopName() {
+        return shopName;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+
+
+}
