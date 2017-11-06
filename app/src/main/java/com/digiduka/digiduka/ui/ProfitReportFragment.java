2631420e9b22daf9ea34031241d9c01ff3309332
ngenovictor <ngenovictor321@gmail.com>
2017-11-06 10:49:43 +0300

    No change to Elvis code

--- app/src/main/java/com/digiduka/digiduka/ui/ProfitReportFragment.java
+++ app/src/main/java/com/digiduka/digiduka/ui/ProfitReportFragment.java
@@ -1,6 +1,4 @@
 package com.digiduka.digiduka.ui;
-
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
@@ -10,7 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import com.digiduka.digiduka.R;
 import com.digiduka.digiduka.adapters.ProductListAdapter;
 import com.digiduka.digiduka.models.Product;
@@ -22,15 +19,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 import org.parceler.Parcels;
-
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-
 /**
  * A simple {@link Fragment} subclass.
  */
@@ -42,18 +36,14 @@ public class ProfitReportFragment extends Fragment {
     private RecyclerView transactionsRecycler;
     private ProductListAdapter mAdapter;
     DateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
-    SimpleDateFormat dateFormatin = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSXXX");
+    SimpleDateFormat dateFormatin = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
     private TextView profitTotal;
     private TextView date;
     private String newDate;
     private String newDate1;
-
-
     public ProfitReportFragment() {
         // Required empty public constructor
     }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -83,14 +73,11 @@ public class ProfitReportFragment extends Fragment {
                         datenew=dateFormat.parse(newDate);
                         datenew1=dateFormat.parse(newDate1);
                     } catch (ParseException e) {
-
                         e.printStackTrace();
                     }
-                    Log.v("date",dateFormat.format(datenew)+"/"+dateFormat.format(date));
-
+                   Log.v("date",dateFormat.format(datenew)+"/"+dateFormat.format(date));
                     if(date.after(datenew)&&date.before(datenew1)) {
                         for (Product product : data.getValue(Transaction.class).getProducts()) {
-
                             if (productname.contains(product.getNameOfProduct())) {
                                 for (Product product1 : products) {
                                     if (product1.getPushId().equals(product.getPushId())) {
@@ -101,12 +88,10 @@ public class ProfitReportFragment extends Fragment {
                                 products.add(product);
                                 productname.add(product.getNameOfProduct());
                             }
-
                             Log.v("product", product.toString());
                         }
                     }
                 }
-
                 date.setText("Profit for  " +dateFormat.format(new Date()));
                 Log.v("size",String.valueOf( products.size()));
                 mAdapter = new ProductListAdapter(getActivity(), products);
@@ -114,18 +99,12 @@ public class ProfitReportFragment extends Fragment {
                 RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
                 transactionsRecycler.setLayoutManager(layoutManager);
                 transactionsRecycler.setHasFixedSize(false);
-
                 gettotalProfit(products);
-
-
             }
-
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
             }
         });
-
     }
     public void gettotalProfit(ArrayList<Product> products){
         int total=0;
@@ -133,7 +112,5 @@ public class ProfitReportFragment extends Fragment {
             total+= ((product.getSellingPrice()-product.getBuyingPrice())*product.getAmount()) ;
         }
         profitTotal.setText("Total profit   Ksh"+String.valueOf(total));
-
     }
-
 }
