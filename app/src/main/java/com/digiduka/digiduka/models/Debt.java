39369fd27460741c8b1f6cdb0be5c6226703a6ac
ngenovictor <ngenovictor321@gmail.com>
2017-11-06 16:23:51 +0300

    Debt Model Model now has parameters and getters and setters

--- app/src/main/java/com/digiduka/digiduka/models/Debt.java
+++ app/src/main/java/com/digiduka/digiduka/models/Debt.java
@@ -1,8 +1,56 @@
 package com.digiduka.digiduka.models;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by victor on 11/6/17.
  */
 
 public class Debt {
+    private String debtorName;
+    private int debtValue;
+    private String pushId;
+    private String dateCreated;
+    private boolean paidUp;
+
+    public Debt(){}
+    public Debt(String debtorsName, int debtAmount){
+        this.debtorName = debtorsName;
+        this.debtValue = debtAmount;
+        //for the date
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+        this.dateCreated = dateFormat.format(new Date());
+
+        this.paidUp = false;
+
+    }
+
+    public String getDebtorName() {
+        return debtorName;
+    }
+
+    public int getDebtValue() {
+        return debtValue;
+    }
+
+    public String getPushId() {
+        return pushId;
+    }
+
+    public String getDateCreated() {
+        return dateCreated;
+    }
+
+    public boolean isPaidUp() {
+        return paidUp;
+    }
+
+    public void setPushId(String pushId) {
+        this.pushId = pushId;
+    }
+
+    public void setPaidUp(boolean paidUp) {
+        this.paidUp = paidUp;
+    }
 }
