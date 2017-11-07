b217de7642d6d280bf0fc2001e0d76a888121b2d
mosoti17 <mosoti17@gmail.com>
2017-11-07 15:05:22 +0300

    edited nav bar layout

--- app/src/main/java/com/digiduka/digiduka/ui/MainActivity.java
+++ app/src/main/java/com/digiduka/digiduka/ui/MainActivity.java
@@ -185,18 +185,12 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.recordDebt) {
+       if (id == R.id.recordDebt) {
             Intent intent = new Intent(MainActivity.this, RecordDebtActivity.class);
             startActivity(intent);
         } else if (id == R.id.nav_send) {
+           Intent intent1 = new Intent(MainActivity.this, GenerateReportsActivity.class);
+           startActivity(intent1);
 
         }
 
